341677d975edc5959cdbf7ee213ccfcd93c7ed95
Pedro <rodriguesdesouza.pedro@gmail.com>
2016-12-05 16:47:42 -0200

    mudei alguns pontos do jquery e coloquei js puro

---
+++ assets/js/main.js
@@ -0,0 +1,55 @@
+          function eliminateRotate()
+          {
+              jQuery('.itemsWrapper').removeClass('rotateHome');
+              jQuery('.itemsWrapper').removeClass('rotateAbout');
+              jQuery('.itemsWrapper').removeClass('rotateSkills');
+              jQuery('.itemsWrapper').removeClass('rotateContact');
+          }
+
+          function eliminateMenu()
+          {
+              jQuery('.mainMenu').removeClass('menuHome');
+              jQuery('.mainMenu').removeClass('menuAbout');
+              jQuery('.mainMenu').removeClass('menuSkills');
+              jQuery('.mainMenu').removeClass('menuContact');
+          }
+
+          function eliminateNameBar()
+          {
+              jQuery('.nameBar').removeClass('nameHome');
+              jQuery('.nameBar').removeClass('nameAbout');
+              jQuery('.nameBar').removeClass('nameSkills');
+              jQuery('.nameBar').removeClass('nameContact');
+          }
+
+          var itensMenu = document.getElementsByClassName("itemMenu");
+
+          function changeMenu(menuSelecionado)
+          {
+            var rotateAttr= menuSelecionado.dataset.rotate
+            eliminateRotate();
+            eliminateMenu();
+            eliminateNameBar();
+            jQuery('.itemsWrapper').addClass('rotate'+rotateAttr);
+            jQuery('.mainMenu').addClass('menu'+rotateAttr);
+            jQuery('.nameBar').addClass('name'+rotateAttr);     
+          }
+
+
+          //Coloca as funcoes em cada click do menu
+          for(var i=0; i<itensMenu.length;i++)
+          {
+            (function () {
+                
+                var menuSelecionado = itensMenu[i];
+
+                itensMenu[i].addEventListener('click', function()
+                {
+                    
+                    changeMenu(menuSelecionado)
+
+                },false);
+
+            }());
+         }
+
